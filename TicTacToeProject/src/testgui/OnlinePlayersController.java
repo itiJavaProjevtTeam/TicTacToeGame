ffcc02eab63b75f87860096c6a3336cb4cd01fbb
Nermeen Abdel Azeem <nermeen.abdel.azeem810@gmail.com>
2020-12-19 00:07:59 +0200

    19/12/2020

--- TicTacToeProject/src/testgui/OnlinePlayersController.java
+++ TicTacToeProject/src/testgui/OnlinePlayersController.java
@@ -26,6 +26,7 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import modes.Game;
 import modes.Player;
@@ -54,7 +55,7 @@ public class OnlinePlayersController extends Thread implements Initializable {
     @FXML
     private TableColumn<Player, String> playerName;
     @FXML
-    private TableColumn<Player,String> PlayerScore;
+    private TableColumn<Player, String> PlayerScore;
 
     @FXML
     private void handleOnlineAction(ActionEvent event) throws IOException {
@@ -65,62 +66,42 @@ public class OnlinePlayersController extends Thread implements Initializable {
 
         window.setScene(s1);
         window.show();
-        // getOnlinePlayers();
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        PlayerScore.setVisible(false);
         try {
             userName = "nermeen";
-
             client = Client.getClient("127.0.0.1", 5007);
             client.sendMessage(userName + ".5." + "PLAYERLIST");
             System.out.println("i am here2");
-            //start();
             OnlinePlayers = client.readResponse();
             System.out.println("The message : " + OnlinePlayers);
             System.out.println("The message sent from the socket was: " + OnlinePlayers);
-            String [] nameScoreList=OnlinePlayers.split("\\.");
-            ArrayList<String> scoreList = new ArrayList<>();
-                    for(int j=13;j<nameScoreList.length;j++){
-                        scoreList.add(nameScoreList[j]);
-                        //System.out.println(scoreList.toString());
-                    }
-            //String [] nameList=nameScoreList[0].split("\\.");
-           // String [] scoreList=nameScoreList[1].split("\\.");
-        ObservableList<Player> elements = FXCollections.observableArrayList();
-        for (int i = 0,j=nameScoreList.length/2; i < nameScoreList.length/2&&j<nameScoreList.length; i++,j++) {
-           // System.out.println(nameScoreList[i]+":"+nameScoreList[j]);
-            //System.out.println(nameScoreList[i]+"  "+scoreList.get(i));
-            Player p = new Player(nameScoreList[i],scoreList.get(i));
-            System.out.println(p.getName()+"    "+p.getscore());
-            elements.add(i, p);
-                    // System.out.println("ele:"+ elements.get(i));
-        }
-      //  System.out.println("ele:" + elements);
-        playerName.setCellValueFactory(new PropertyValueFactory<Player,String>("name"));
+            String[] nameScoreList = OnlinePlayers.split("\\.");
+            ObservableList<Player> elements = FXCollections.observableArrayList();
+            for (int i = 0, j = nameScoreList.length / 2; i < nameScoreList.length / 2 && j < nameScoreList.length; i++, j++) {
+                elements.add(i, new Player(nameScoreList[i], nameScoreList[j]));
+            }
+            playerName.setCellValueFactory(new PropertyValueFactory<Player, String>("name"));
+            PlayerScore.setCellValueFactory(new PropertyValueFactory<Player, String>("pScore"));
+            TableP.setItems(elements);
 
-        PlayerScore.setCellValueFactory(new PropertyValueFactory<Player,String>("pScore"));
-        TableP.setItems(elements);
-//        System.out.println(TableP.getSelectionModel().getSelectedItem().getscore());
         } catch (IOException ex) {
-        ex.printStackTrace();        }
+            ex.printStackTrace();
+        }
 
     }
+  
 
     void setUserNameScore(String userName, String score) {
         this.userName = userName;
         this.scoreClient = score;
     }
 
-    public void run() {
-        try {
-            OnlinePlayers = client.readResponse();
-            System.out.println("The message : " + OnlinePlayers);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-
+    @FXML
+    private void OnMousePressed(MouseEvent event) {
     }
 
 }
